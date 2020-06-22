65525ccb7cd0f4634573e63d72d341df7b8f45c0
Matei Mandache <matei.mandache@gmail.com>
2020-06-22 21:33:56 +0100

    Improved look and function of answer area and question text

--- dev/mathsQuiz.js
+++ dev/mathsQuiz.js
@@ -1,6 +1,15 @@
+var score = 0;
+var animationTime = 1000;
+var penalty = 2;
+var maxNumber = 12;
+var timer = false;
+var time = 10;
+
 function start() {
   document.getElementById("settings-and-start").style = "display: none;";
-  document.getElementById("quiz-area").style = "display: block;";
+  document.getElementById("quiz-area").style = "display: flex;";
+  // Clear answer box in case it already contains stuff
+  document.getElementById("answer-box").value = "";
 }
 
 function resize(currentScore, finalScore, duration, callback) {
@@ -26,10 +35,6 @@ function resize(currentScore, finalScore, duration, callback) {
   }
 }
 
-var score = 0;
-var animationTime = 1000;
-var penalty = 2;
-
 function updateScore(value) {
   // Score is capped between 10 and -10
   value = Math.min(Math.max(value, -10), 10);
@@ -41,6 +46,7 @@ function updateScore(value) {
 function doNextThing() {
   var sheet = document.styleSheets[0]
   if (score == 10) {
+    document.getElementById("question").innerHTML = "You won!";
     sheet.insertRule(`#you {
       animation-timing-function: linear;
       animation-name: winning-dance;
@@ -48,6 +54,8 @@ function doNextThing() {
       animation-iteration-count: infinite;
     }`, sheet.cssRules.length);
   } else if (score == -10) {
+    document.getElementById("question").innerHTML =
+                                      "Quizasaurus has defeated you!"
     document.styleSheets[0].insertRule(`#dinosaur {
       animation-timing-function: linear;
       animation-name: winning-dance;
@@ -57,15 +65,20 @@ function doNextThing() {
   } else {
     currentQuestion = new Question();
     currentQuestion.display()
+    document.getElementById("submit-button").disabled = false;
   }
 }
 
 function evaluateAnswer() {
+  document.getElementById("submit-button").disabled = true;
   var receivedAnswer = document.getElementById("answer-box").value;
+  document.getElementById("answer-box").value = "";
   var actualAnswer = currentQuestion.answer;
   if (receivedAnswer == actualAnswer) {
+    document.getElementById("question").innerHTML = "Correct!"
     updateScore(score + 1);
   } else {
+    document.getElementById("question").innerHTML = "Wrong!"
     updateScore(score - penalty);
   }
 }
@@ -74,8 +87,8 @@ var currentQuestion = new Question();
 currentQuestion.display();
 
 function Question() {
-  var num1 = 2 + Math.floor(11 * Math.random());
-  var num2 = 2 + Math.floor(11 * Math.random());
+  var num1 = 2 + Math.floor((maxNumber-1) * Math.random());
+  var num2 = 2 + Math.floor((maxNumber-1) * Math.random());
   this.prompt = `What is ${num1} times ${num2}?`;
   this.answer = num1 * num2;
   this.display = function() {
@@ -84,3 +97,9 @@ function Question() {
       .addEventListener("click", evaluateAnswer);
   }
 }
+
+document.getElementById("answer-box").addEventListener("keydown", event => {
+  if (event.keyCode === 13) {
+    document.getElementById("submit-button").click();
+  }
+})
--- mathsQuiz.js
+++ mathsQuiz.js
@@ -1,8 +1,17 @@
 "use strict";
 
+var score = 0;
+var animationTime = 1000;
+var penalty = 2;
+var maxNumber = 12;
+var timer = false;
+var time = 10;
+
 function start() {
   document.getElementById("settings-and-start").style = "display: none;";
-  document.getElementById("quiz-area").style = "display: block;";
+  document.getElementById("quiz-area").style = "display: flex;";
+  // Clear answer box in case it already contains stuff
+  document.getElementById("answer-box").value = "";
 }
 
 function resize(currentScore, finalScore, duration, callback) {
@@ -29,10 +38,6 @@ function resize(currentScore, finalScore, duration, callback) {
   }
 }
 
-var score = 0;
-var animationTime = 1000;
-var penalty = 2;
-
 function updateScore(value) {
   // Score is capped between 10 and -10
   value = Math.min(Math.max(value, -10), 10);
@@ -44,21 +49,28 @@ function updateScore(value) {
 function doNextThing() {
   var sheet = document.styleSheets[0];
   if (score == 10) {
+    document.getElementById("question").innerHTML = "You won!";
     sheet.insertRule("#you {\n      animation-timing-function: linear;\n      animation-name: winning-dance;\n      animation-duration: 1.2s;\n      animation-iteration-count: infinite;\n    }", sheet.cssRules.length);
   } else if (score == -10) {
+    document.getElementById("question").innerHTML = "Quizasaurus has defeated you!";
     document.styleSheets[0].insertRule("#dinosaur {\n      animation-timing-function: linear;\n      animation-name: winning-dance;\n      animation-duration: 1.2s;\n      animation-iteration-count: infinite;\n    }", sheet.cssRules.length);
   } else {
     currentQuestion = new Question();
     currentQuestion.display();
+    document.getElementById("submit-button").disabled = false;
   }
 }
 
 function evaluateAnswer() {
+  document.getElementById("submit-button").disabled = true;
   var receivedAnswer = document.getElementById("answer-box").value;
+  document.getElementById("answer-box").value = "";
   var actualAnswer = currentQuestion.answer;
   if (receivedAnswer == actualAnswer) {
+    document.getElementById("question").innerHTML = "Correct!";
     updateScore(score + 1);
   } else {
+    document.getElementById("question").innerHTML = "Wrong!";
     updateScore(score - penalty);
   }
 }
@@ -67,8 +79,8 @@ var currentQuestion = new Question();
 currentQuestion.display();
 
 function Question() {
-  var num1 = 1 + Math.floor(12 * Math.random());
-  var num2 = 1 + Math.floor(12 * Math.random());
+  var num1 = 2 + Math.floor((maxNumber - 1) * Math.random());
+  var num2 = 2 + Math.floor((maxNumber - 1) * Math.random());
   this.prompt = "What is " + num1 + " times " + num2 + "?";
   this.answer = num1 * num2;
   this.display = function () {
@@ -76,3 +88,9 @@ function Question() {
     document.getElementById("submit-button").addEventListener("click", evaluateAnswer);
   };
 }
+
+document.getElementById("answer-box").addEventListener("keydown", function (event) {
+  if (event.keyCode === 13) {
+    document.getElementById("submit-button").click();
+  }
+});
